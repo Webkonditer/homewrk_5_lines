59adb224a1ec1bfa92ddaed62411e5b91e10664e
6564747347456845 <webkonditer@yandex.ru>
2022-05-06 22:14:10 +0400

    Задания повышенной сложности

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -40,5 +42,72 @@ public class Main {
         fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace('ё','е');
         System.out.println("Данные ФИО сотрудника  — " + fullName);
+
+        //Задания повышенной сложности
+
+        //Задание 5
+        //К нам снова обратились за помощью, но теперь уже для того, чтобы написать алгоритм, разбивающий одну строку с Ф. И. О. на три — на фамилию, имя и отчество.
+        System.out.println("----------Задание 5:-------------");
+
+        fullName = "Ivanov Ivan Ivanovich";
+        int firstWhitespace = fullName.indexOf(' ');
+        int secondWhitespace = fullName.indexOf(' ', firstWhitespace + 1);
+        firstName = fullName.substring(firstWhitespace + 1, secondWhitespace);
+        lastName = fullName.substring(0, firstWhitespace + 1);
+        middleName = fullName.substring(secondWhitespace + 1);
+        System.out.println("Имя сотрудника — " + firstName);
+        System.out.println("Фамилия сотрудника — " + lastName);
+        System.out.println("Отчество сотрудника — " + middleName);
+
+        //Задание 6
+        //Периодически данные в наших регистрах заполняются неверно, и Ф. И. О. сотрудников записывают со строчных букв.
+        //Такую оплошность нужно исправить, написав программу, которая преобразует написанное со строчных букв Ф. И. О. в правильный формат.
+
+        System.out.println("----------Задание 6:-------------");
+
+        fullName = "ivanov ivan ivanovich";
+        firstWhitespace = fullName.indexOf(' ');
+        secondWhitespace = fullName.indexOf(' ', firstWhitespace + 1);
+        char[] charedFullName = fullName.toCharArray();
+        charedFullName[0] = Character.toUpperCase(charedFullName[0]);
+        charedFullName[firstWhitespace + 1] = Character.toUpperCase(charedFullName[firstWhitespace + 1]);
+        charedFullName[secondWhitespace + 1] = Character.toUpperCase(charedFullName[secondWhitespace + 1]);
+        fullName = new String(charedFullName);
+        System.out.println("Верное написание Ф. И. О. сотрудника с заглавных букв — " + fullName);
+
+        //Задание 7
+        //Имеется две строки.
+        //Первая: "135"
+        //Вторая: "246"
+        //Соберите из двух строк одну, содержащую данные "123456".
+        //Использовать сортировку нельзя.
+
+        System.out.println("----------Задание 7:-------------");
+
+        String firstLine = "135";
+        String secondLine = "246";
+        StringBuilder newLine = new StringBuilder();
+        for (int i = 0; i < firstLine.length(); i++) {
+            newLine.insert(newLine.length(), firstLine.charAt(i));
+            newLine.insert(newLine.length(), secondLine.charAt(i));
+        }
+        System.out.println("Данные строки — " + newLine);
+
+        //Задание 8
+        //Дана строка из букв английского алфавита "aabccddefgghiijjkk".
+        //Нужно найти и напечатать буквы, которые дублируются в строке.
+        //Обратите внимание, что строка отсортирована, т. е. дубли идут друг за другом.
+        //В итоге в консоль должен быть выведен результат программы: "acdgijk".
+
+        System.out.println("----------Задание 8:-------------");
+
+        String nextLine = "aabccddefgghiijjkk";
+        StringBuilder finishLine = new StringBuilder();
+        for (int i = 0; i < nextLine.length()-1; i++) {
+            if (nextLine.charAt(i) == nextLine.charAt(i+1)) {
+                finishLine.insert(finishLine.length(), nextLine.charAt(i));
+            }
+        }
+        System.out.println("Данные строки — " + finishLine);
     }
 }
